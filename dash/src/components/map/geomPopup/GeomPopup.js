5754a046620d32562bd192e93aab833b20f9be76
mevanmaele <mvanmaele@talusanalytics.com>
2019-09-30 19:48:56 -0400

    WIP -- tooltips

--- dash/src/components/map/geomPopup/GeomPopup.js
+++ dash/src/components/map/geomPopup/GeomPopup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 import styles from './geomPopup.module.scss'
 
@@ -12,43 +13,61 @@ const GeomPopup = ({ popupData }) => {
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.titleContainer}>
-          <p>
+          <p className={styles.stateName}>
             {popupData['fill']['place_name']}
           </p>
-          <p>
-            {new Date().toLocaleString('en-us', {
-              month: 'long',
-              year: 'numeric',
-              day: 'numeric'
-            })}
-          </p>
-
+          {
+            // <p>
+            //   {new Date().toLocaleString('en-us', {
+            //     month: 'long',
+            //     year: 'numeric',
+            //     day: 'numeric'
+            //   })}
+            // </p>
+          }
         </div>
       </div>
       <div>
-        <div>
-          <p>
-            Vaccination coverage (1-2 y/o)
-          </p>
-          <p>
-            {parseFloat(popupData['fill']['value']).toFixed(0)+"%"}
-          </p>
-          <p>
-            Measles Cases
-          </p>
-          <p>
-            {popupData['bubble']['value']}
-          </p>
-          <p>
-            Month over month
-          </p>
-          <p>
-            {popupData['trend']['percent_change']}
-          </p>
+        <div className={styles.data}>
+          {
+            [
+              {
+                slug: 'cases',
+                label: 'Measles cases reported',
+                value: popupData['bubble']['value'] + ' people', // TODO comma sep int
+                delta: popupData['trend']['percent_change'],
+                deltaLabel: 'increase from prior 30 days', // TODO inc/dec dynamically
+                notAvail: false,
+              },
+              {
+                slug: 'prevalence',
+                label: 'Prevalence of measles in population',
+                value: 'Data not available', // TODO comma-sep int
+                notAvail: true,
+              },
+              {
+                slug: 'vacc-coverage',
+                label: 'Vaccination coverage',
+                value: parseFloat(popupData['fill']['value']).toFixed(0)+"% of people",
+                notAvail: false,
+              },
+            ].map(d =>
+              <div className={classNames(styles[d.slug], styles.datum)}>
+                <p className={classNames(styles[d.slug], styles.label)}>{d.label}</p>
+                <p className={classNames(
+                  styles[d.slug],
+                  styles.value,
+                  {
+                    [styles['notAvail']]: d.notAvail,
+                  },
+                )}>{d.value}</p>
+              </div>
+            )
+          }
         </div>
         <div className={styles.buttons}>
           <Link to={detailsPath}>
-            <button>Country detail</button>
+            <button>View country</button>
           </Link>
         </div>
       </div>
