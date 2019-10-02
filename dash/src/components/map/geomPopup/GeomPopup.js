9a6a277637e98f58910b20648c1448d9307ec311
mevanmaele <mvanmaele@talusanalytics.com>
2019-10-02 15:59:51 -0400

    Rmv material-ui

--- dash/src/components/map/geomPopup/GeomPopup.js
+++ dash/src/components/map/geomPopup/GeomPopup.js
@@ -33,11 +33,11 @@ const GeomPopup = ({ popupData }) => {
         {
           <p className={styles.dataDateStamp}>
             {
-              'as of ' + new Date('7/01/2019').toLocaleString('en-us', { // TODO correctly
+              'Best available data for ' + new Date('7/01/2019').toLocaleString('en-us', { // TODO correctly
                 month: 'long',
                 year: 'numeric',
               })
-          }
+            }
           </p>
         }
         <div className={styles.data}>
@@ -49,20 +49,22 @@ const GeomPopup = ({ popupData }) => {
                 value: popupData['bubble']['value'] + ' people', // TODO comma sep int
                 delta: popupData['trend']['percent_change'],
                 deltaLabel: 'increase from prior 30 days', // TODO inc/dec dynamically
-                notAvail: false,
+                notAvail: !popupData['bubble']['value'],
                 dataSource: popupData['bubble']['data_source'],
               },
               {
                 slug: 'prevalence',
                 label: 'Prevalence of measles',
                 value: 'Data not available', // TODO comma-sep int
-                notAvail: true,
+                notAvail: true, // TODO dynamically
               },
               {
                 slug: 'vacc-coverage',
                 label: 'Vaccination coverage',
                 value: parseFloat(popupData['fill']['value']).toFixed(0)+"% of infants",
-                notAvail: false,
+                dataSource: popupData['fill']['data_source'],
+                dataSourceLastUpdated: new Date (popupData['fill']['updated_at']),
+                notAvail: false, // TODO dynamically
               },
             ].map(d =>
               <div className={classNames(styles[d.slug], styles.datum)}>
@@ -73,9 +75,25 @@ const GeomPopup = ({ popupData }) => {
                   {
                     [styles['notAvail']]: d.notAvail,
                   },
-                )}>{d.value}</p>
+                )}>
+                  {d.notAvail ? 'Data not available' : d.value}
+                </p>
                 {
-                  d.dataSource && <p className={'dataSource'}>Source: {d.dataSource}</p>
+                  false && d.delta && !d.notAvail && <p className={styles.delta}> // TODO remove false when done
+                    Delta
+                  </p>
+                }
+                {
+                  (d.dataSource && !d.notAvail) &&
+                    <p className={'dataSource'}>
+                      Source: {d.dataSource}{ d.dataSourceLastUpdated && false && ( // TODO remove "false" when this field is ready
+                          ' as of ' + new Date(d.dataSourceLastUpdated).toLocaleString('en-us', { // TODO correctly
+                            month: 'long',
+                            year: 'numeric',
+                          })
+                        )
+                      }
+                    </p>
                 }
               </div>
             )
