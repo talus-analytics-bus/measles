53ff58fe0a18beefc4956ab83d17e92d3bca8592
mevanmaele <mvanmaele@talusanalytics.com>
2019-11-12 11:52:29 -0500

    add yearly report data option for countries that report measles data yearly instead of monthly, to tooltip

---
+++ dash/src/components/misc/YearlyReport.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styles from './yearlyreport.module.scss';
+import Util from './Util.js';
+
+/**
+ * Yearly metric report for countries that have irregular WHO data reporting.
+ * Simple table of metric by year with data source.
+ * @method YearlyReport
+ */
+const YearlyReport = (props) => {
+  const yearlyReportData = {
+    'caseload_totalpop': {
+      'VE': {
+        values: [
+          {"value":449,"year":2019},
+          {"value":5668,"year":2018},
+          {"value":727,"year":2017},
+          {"value":1,"year":2016},
+          {"value":0,"year":2015},
+          {"value":0,"year":2014},
+          {"value":0,"year":2013},
+          {"value":1,"year":2012},
+          {"value":0,"year":2011}
+        ],
+        data_source: 'WHO Measles Surveillance Data as of Oct 2019',
+      },
+    },
+    'incidence_yearly': {
+      'VE': {
+        values: [{"value":15.74564078,"year":2019},
+        {"value":196.2120346,"year":2018},
+        {"value":24.72580544,"year":2017},
+        {"value":0.033499436,"year":2016},
+        {"value":0,"year":2015},
+        {"value":0,"year":2014},
+        {"value":0,"year":2013},
+        {"value":0.034058986,"year":2012},
+        {"value":0,"year":2011}],
+        data_source: 'WHO Measles Surveillance Data and UN World Population Prospects as of Sep 2019',
+      },
+    },
+  }
+  const paramName = props.metric === 'caseload_totalpop' ? 'caseload_totalpop'
+    : 'incidence_yearly';
+  const metricParams = Util.getMetricChartParams(paramName);
+  const data = yearlyReportData[paramName][props.place_iso];
+  const metricName = props.metric === 'caseload_totalpop' ? 'Measles caseloads'
+    : 'Measles incidence rates';
+  return (
+    <div
+      className={styles.yearlyReport}
+    >
+      <p>
+        {metricName} for {props.place_name} are currently only available by year. Data for all years on record are provided in the table below.
+      </p>
+      <table>
+          <tr>
+            <td>
+              Year
+            </td>
+            <td>
+              {metricParams.name} ({metricParams.units})
+            </td>
+          </tr>
+        {
+          data.values.map(d =>
+            <tr>
+              <td>
+                {d.year}
+              </td>
+              <td>
+                {metricParams.tickFormatLong(d.value)}
+              </td>
+            </tr>
+          )
+        }
+      </table>
+      <div className={'dataSource'}>
+        Source: { data.data_source }
+      </div>
+    </div>
+  );
+};
+
+export default YearlyReport;
