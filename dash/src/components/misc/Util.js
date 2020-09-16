1a6776522507cf300d43f294f3e74f0462975ee2
mevanmaele <mvanmaele@talusanalytics.com>
2020-09-16 16:15:42 -0400

    Update global page date: Aug 2020

--- dash/src/components/misc/Util.js
+++ dash/src/components/misc/Util.js
@@ -600,7 +600,7 @@ Util.formatDatetimeApi = dt => {
 Util.globalMaxDate = () => {
   const globalMaxDate = new Date('2000/01/01')
   globalMaxDate.setDate(1)
-  globalMaxDate.setMonth(6) // Jul
+  globalMaxDate.setMonth(7) // Aug
   globalMaxDate.setYear(2020)
   return globalMaxDate
 }
