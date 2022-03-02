0967dfaa52d9917d3cc0ce60f72dfab5943c51e9
mvanmaele <mvanmaele@talusanalytics.com>
2022-03-02 12:10:30 -0500

    Format code

--- public-api/api/db.py
+++ public-api/api/db.py
@@ -23,24 +23,29 @@ app = Flask(__name__)
 CORS(app)
 
 # Load Flask API
-api = Api(app, version='0.1')
+api = Api(app, version="0.1")
 
-app.config['NAME'] = 'Response Elements API'
-app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
-app.config['SWAGGER_UI_REQUEST_DURATION'] = True
-app.config['RESTPLUS_MASK_SWAGGER'] = False
-app.config['pg_config'] = {}
+app.config["NAME"] = "Response Elements API"
+app.config["SWAGGER_UI_DOC_EXPANSION"] = "list"
+app.config["SWAGGER_UI_REQUEST_DURATION"] = True
+app.config["RESTPLUS_MASK_SWAGGER"] = False
+app.config["pg_config"] = {}
 
 # Import standard config class to configure the database.
-appconfig = Config('dbconfig.ini')
+appconfig = Config("dbconfig.ini")
 
 
 # Setup for Pony ORM ###########################################################
 
 # Bind database object to the target database (postgres assumed) using the
 # dbconfig.ini and command line-derived configuration arguments.
-db.bind(provider='postgres', user=appconfig.db['user'], password=appconfig.db['password'],
-        host=appconfig.db['host'], database=appconfig.db['dbname'])
+db.bind(
+    provider="postgres",
+    user=appconfig.db["user"],
+    password=appconfig.db["password"],
+    host=appconfig.db["host"],
+    database=appconfig.db["dbname"],
+)
 
 # Generate mapping (create tables if they don't already exist) to store data.
 # Change this argument to True if the tables you need don't yet exist.
