d82e183b8f3a8f02fda1c146d1d41c531033a09b
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-10-18 15:44:25 -0300

    Removing duplicate instantiation of Flask app

--- public-api/application.py
+++ public-api/application.py
@@ -1,11 +1,11 @@
-from flask import Flask
+# from flask import Flask
 from api import app as application
 
-application = app = Flask(__name__)
+# application = app = Flask(__name__)
 
 
 def main():
-    application = app = Flask(__name__)
+    # application = app = Flask(__name__)
     application.run(host="localhost", port=5002, debug=True)
 
 
